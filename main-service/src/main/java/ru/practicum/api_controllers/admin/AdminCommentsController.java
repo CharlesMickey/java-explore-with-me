fff69b622057e0f3bffc2630ba9085e4df92b850
Artur Chukhnenko <CharlesMickey.ru@yandex.ru>
2024-03-10 19:18:23 +0300

    Feat: добавить заготовки контроллеров

---
+++ main-service/src/main/java/ru/practicum/api_controllers/admin/AdminCommentsController.java
@@ -0,0 +1,13 @@
+package ru.practicum.api_controllers.admin;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = "/admin/comments")
+@Slf4j
+public class AdminCommentsController {
+}
